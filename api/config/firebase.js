52fbc4bddf56699472d7cdb72fc0e04c21d41ce5
Seifeldin Amr <seifeldinamr862@gmail.com>
2025-08-06 17:26:55 +0300

    tested auth

--- api/config/firebase.js
+++ api/config/firebase.js
@@ -9,8 +9,6 @@ const serviceAccount = {
   private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
 };

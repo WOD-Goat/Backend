1f4677383f370ce55a587409218fd774a6cca123
Seifeldin Amr <seifeldinamr862@gmail.com>
2025-08-19 16:19:41 +0300

    made all user data not optional

--- types/user.types.ts
+++ types/user.types.ts
@@ -1,12 +1,12 @@
 export interface UserData {
   uid?: string;
   fullName: string;
-  nickname?: string;
-  mobileNumber?: string;
-  gender?: 'male' | 'female' | '';
-  weight?: number | null;
-  age?: number | null;
-  height?: number | null;
+  nickname: string;
+  mobileNumber: string;
+  gender: 'male' | 'female' | '';
+  weight: number | null;
+  age: number | null;
+  height: number | null;
   email: string;
   isTrainer: boolean;
   createdAt?: string;
